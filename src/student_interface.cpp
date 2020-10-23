9520908faab3f7d649e7dde0ac37795073ee5db1
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2020-10-23 17:50:56 -0400

    Assignment1

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -3,47 +3,102 @@
 
 #include <stdexcept>
 #include <sstream>
+
+#include <vector>
+#include <atomic>
+#include <unistd.h>
+
+#include <experimental/filesystem>
+#include <sstream>
+
+
+
+
 namespace student {
 
  void loadImage(cv::Mat& img_out, const std::string& config_folder){  
-   throw std::logic_error( "STUDENT FUNCTION - LOAD IMAGE - NOT IMPLEMENTED" );
- }
+  static bool initialized = false;
+  static std::vector<cv::String> img_list; // list of images to load
+  static size_t idx = 0;  // idx of the current img
+  static size_t function_call_counter = 0;  // idx of the current img
+  const static size_t freeze_img_n_step = 30; // hold the current image for n iteration
+  static cv::Mat current_img; // store the image for a period, avoid to load it from file every time
+  
+  if(!initialized){
+    const bool recursive = false;
+    // Load the list of jpg image contained in the config_folder/img_to_load/
+    cv::glob(config_folder + "/img_to_load/*.jpg", img_list, recursive);
+    
+    if(img_list.size() > 0){
+      initialized = true;
+      idx = 0;
+      current_img = cv::imread(img_list[idx]);
+      function_call_counter = 0;
+    }else{
+      initialized = false;
+    }
+  }
+  
+  if(!initialized){
+    throw std::logic_error( "Load Image can not find any jpg image in: " +  config_folder + "/img_to_load/");
+    return;
+  }
+  
+  img_out = current_img;
+  function_call_counter++;  
+  
+  // If the function is called more than N times load increment image idx
+  if(function_call_counter > freeze_img_n_step){
+    function_call_counter = 0;
+    idx = (idx + 1)%img_list.size();    
+    current_img = cv::imread(img_list[idx]);
+  }
+}
 
  void genericImageListener(const cv::Mat& img_in, std::string topic, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - IMAGE LISTENER - NOT CORRECTLY IMPLEMENTED" );
+	std::cout<<"Student Interface: genericImageListener"<<std::endl;
+	static size_t id = 0;
+  	static bool init = false;
+  	static std::string folder_path;
+
+  	if(!init){      
+      bool exist = true;
+      int i = 0;
+      while(exist && i < 1000){
+        std::stringstream ss;
+        ss << config_folder << "/camera_image" << std::setw(3) << std::setfill('0') << i << "/";
+  		  folder_path = ss.str();
+
+        exist = std::experimental::filesystem::exists(folder_path);
+
+        i++;        
+      }
+      
+      if(i > 999 || !std::experimental::filesystem::create_directories(folder_path)){
+        throw std::logic_error( "NO EMTY FOLDER" );
+      }
+
+  		init = true;
+  	}
+    	    
+    cv::imshow( topic, img_in);
+    char c;
+    c = cv::waitKey(30);
+    
+    std::stringstream img_file;
+    switch (c) {    	
+		case 's':		
+			img_file << folder_path << std::setfill('0') 
+					<< std::setw(3)  << (id++) << ".jpg";
+		 	cv::imwrite( img_file.str(), img_in );
+
+		 	std::cout << "Saved image " << img_file.str() << std::endl;
+		 	break;
+		default:
+				break;
+    }
   }
 
-  bool extrinsicCalib(const cv::Mat& img_in, std::vector<cv::Point3f> object_points, const cv::Mat& camera_matrix, cv::Mat& rvec, cv::Mat& tvec, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - EXTRINSIC CALIB - NOT IMPLEMENTED" );   
-  }
-
-  void imageUndistort(const cv::Mat& img_in, cv::Mat& img_out, 
-          const cv::Mat& cam_matrix, const cv::Mat& dist_coeffs, const std::string& config_folder){
-
-    throw std::logic_error( "STUDENT FUNCTION - IMAGE UNDISTORT - NOT IMPLEMENTED" );  
-
-  }
-
-  void findPlaneTransform(const cv::Mat& cam_matrix, const cv::Mat& rvec, 
-                        const cv::Mat& tvec, const std::vector<cv::Point3f>& object_points_plane, 
-                        const std::vector<cv::Point2f>& dest_image_points_plane, 
-                        cv::Mat& plane_transf, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - FIND PLANE TRANSFORM - NOT IMPLEMENTED" );  
-  }
-
-
-void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf, 
-            const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - UNWRAP - NOT IMPLEMENTED" );   
-  }
-
-  bool processMap(const cv::Mat& img_in, const double scale, std::vector<Polygon>& obstacle_list, std::vector<std::pair<int,Polygon>>& victim_list, Polygon& gate, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - PROCESS MAP - NOT IMPLEMENTED" );   
-  }
-
-  bool findRobot(const cv::Mat& img_in, const double scale, Polygon& triangle, double& x, double& y, double& theta, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - FIND ROBOT - NOT IMPLEMENTED" );    
-  }
 
   bool planPath(const Polygon& borders, const std::vector<Polygon>& obstacle_list, const std::vector<std::pair<int,Polygon>>& victim_list, const Polygon& gate, const float x, const float y, const float theta, Path& path){
     throw std::logic_error( "STUDENT FUNCTION - PLAN PATH - NOT IMPLEMENTED" );     
